ed9a9e5b21bc72832f723117d1c3d8cd99abbe07
unknown <hazardovic1@gmail.com>
2024-05-26 21:52:31 +0300

    stash

---
+++ rentwise-landlord/app/(tabs)/settings.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text } from "react-native";
+
+export default function TabTwoScreen() {
+  return <Text style={styles.text}>Explore</Text>;
+}
+
+const styles = StyleSheet.create({
+  text: {
+    color: "white",
+  },
+});
